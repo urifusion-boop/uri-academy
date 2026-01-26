6fa0bcad2620d1624802b4ad778d910b92b95557
Shore <koyaoluwashorefunmi@Koyas-MacBook-Air.local>
2026-01-26 22:10:10 +0100

    fix: switch to local backend proxy for development

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.uricreative.com:8448',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       },
